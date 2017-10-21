6dc513fbd6a6252534ba5951a4a0d57188721687
Unknown <jodydarien@yahoo.com>
2017-10-20 20:53:57 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -1,6 +1,21 @@
+/*window.onload=function(){
+  
+  	document.getElementById("boundary1").addEventListener("mouseover",function(){
+  		this.className="boundary youlose";
+  		});
+};*/
+	
 window.onload=function(){
 
-	document.getElementById("boundary1").addEventListener("mouseover",function(){
-		this.className="boundary youlose";
-		});
-}
+	var border=document.querySelectorAll("div.boundary");
+	
+	for (var i=0; i<border.length;i++){
+		border[i].onmouseover=function(){
+				var border=document.querySelectorAll("div.boundary");
+				for (var i=0; i<border.length-1;i++){
+					border[i].className="boundary youlose";
+					};
+			};
+		};
+};
+		
